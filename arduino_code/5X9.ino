b9294ca33b7b2e339858abc96972514b37826a37
liuxiaoyang98 <liuxiaoyang98@gmail.com>
2022-11-25 15:28:39 +0900

    Update 5X9.ino

--- arduino_code/5X9.ino
+++ arduino_code/5X9.ino
@@ -3,6 +3,7 @@
 #define Y2  21
 #define P19 20
 #define P18 19
+
 #define P9  18
 #define P10 17
 #define P8  16
@@ -16,36 +17,38 @@ uint8_t adc[6]={A0,A1,A2,A3,A4};
 uint8_t io[10]={Y1,Y2,P19,P18,P9,P10,P8,P7,P6};
 uint16_t gpio[10][6];
 
-//void sel_x(uint8_t x){
-//  digitalWrite(XA0, x & 1);
-//  digitalWrite(XA1, (x >> 1) & 1);
-//  digitalWrite(XA2, (x >> 2) & 1);
-//  digitalWrite(XA3, (x >> 3) & 1);
-//}
-//void sel_y(uint8_t y){
-//  digitalWrite(YA0, y & 1);
-//  digitalWrite(YA1, (y >> 1) & 1);
-//  digitalWrite(YA2, (y >> 2) & 1);
-//  digitalWrite(YA3, (y >> 3) & 1);
-//}
-void get_adc(uint8_t pin) {
-  delayMicroseconds(50);
-  uint16_t ret[6];
-  
-  uint16_t Max=0,Min=0xffff,sum=0;
-  
-  for (uint8_t i=0;i<5;i++){
-    for(uint8_t i0=0;i0<6;i++){//测5次取平均
-      uint16_t temp=analogRead(adc[i]);
-      sum=sum+temp;
-      if(Max<temp)Max=temp;
-      if(Min>temp)Min=temp;
-    }
-    gpio[pin][i]=(sum-Max-Min)/3;
-  }
+void get_adc() {
+  Serial.print("good");
+//  delayMicroseconds(50);
+//  
+//  uint16_t ret[6];
+//  
+//  for(uint8_t pin=0;pin<9;pin++){//9个gpio
+//
+//    pinMode(io[pin],OUTPUT);
+//    digitalWrite(io[pin],HIGH);
+//    
+//    uint16_t Max=0,Min=0xffff,sum=0;
+//
+//    
+//      for (uint8_t i=0;i<5;i++){//5个adc
+//        for(uint8_t i0=1;i0<6;i++){//测5次取平均
+//          uint16_t temp=analogRead(adc[i]);
+//          sum=sum+temp;
+//          if(Max<temp)Max=temp;
+//          if(Min>temp)Min=temp;
+//        }
+//    gpio[pin][i]=(sum-Max-Min)/3;
+//    }
+//
+//    
+//    pinMode(io[pin],INPUT);
+//  }
+
 }
 
 void setup() {
+  Serial.end();
   // put your setup code here, to run once:
   Serial.begin(115200);
 //  pinMode(LED_CE,OUTPUT);
@@ -55,33 +58,67 @@ void setup() {
 //  pinMode(A3,OUTPUT);
 //  pinMode(A4,OUTPUT);
   
-  pinMode(Y1,INPUT);pinMode(Y2,INPUT);
-  pinMode(P19,INPUT);pinMode(P18,INPUT);  
-  pinMode(P10,INPUT);pinMode(P9,INPUT);
-  pinMode(P8,INPUT);pinMode(P7,INPUT);
-  pinMode(P6,INPUT);
+  Serial.print("1good");
+  pinMode(22,INPUT);//good
+//  pinMode(21,INPUT);//TX不管它
+  pinMode(P19,INPUT);//good
+  pinMode(P18,INPUT); //good
 
   
-  pinMode(CS,OUTPUT);
-  pinMode(Y0,OUTPUT);
-  digitalWrite(CS,HIGH); //disable out
-  digitalWrite(Y0,LOW);
+//  pinMode(P10,INPUT);//代码通过之后到loop()就断了
+  pinMode(P9,INPUT);//good
+//  pinMode(P8,INPUT);//代码走到这里就断了
+//  pinMode(P7,INPUT);//代码走到这里就断了
+//  pinMode(P6,INPUT);//代码通过之后到loop()就断了
+
+  Serial.print("2good"); 
+  Serial.println(""); 
+//
+//  
+//  pinMode(CS,OUTPUT);
+//  pinMode(Y0,OUTPUT);
+//  digitalWrite(CS,HIGH); //disable out
+//  digitalWrite(Y0,LOW);
 }
 void loop() {
   // put your main code here, to run repeatedly:
   delay(1000);
   //digitalWrite(Y0,HIGH);
-  for(uint8_t i=0;i<9;i++){
-    pinMode(io[i],OUTPUT);
-    digitalWrite(io[i],HIGH);
-    get_adc(i);
-    pinMode(io[i],INPUT);
-  }
-  for(uint8_t i=0;i<6;i++){
-      for(uint8_t i0=0;i<10;i++)
-        Serial.print(gpio[i][i0]);
-      Serial.println();
-  }
   
-  digitalWrite(Y0,LOW);
+  Serial.print("loop");
+  get_adc();
+
+//2 3 5
+
+int lop[]={2,3,5};
+for(int pin=0;pin<3;pin++){
+    pinMode(io[pin],OUTPUT);
+    digitalWrite(io[pin],HIGH);
+    
+    uint16_t Max=0,Min=0xffff,sum=0;
+
+    
+      for (uint8_t i=0;i<5;i++){//5个adc
+        for(uint8_t i0=1;i0<6;i++){//测5次取平均
+          uint16_t temp=analogRead(adc[i]);
+          sum=sum+temp;
+          if(Max<temp)Max=temp;
+          if(Min>temp)Min=temp;
+        }
+    gpio[lop][i]=(sum-Max-Min)/3;
+    }
+
+    
+    pinMode(io[pin],INPUT);
+}
+  
+
+//  
+//  for(uint8_t i=0;i<6;i++){//display
+//      for(uint8_t i0=0;i0<10;i0++)
+//        Serial.print(gpio[i][i0]);
+//      Serial.println();
+//  }
+  
+//  digitalWrite(Y0,LOW);
 }
